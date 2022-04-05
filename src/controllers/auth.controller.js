d2fe0c4932dce0340a4af7b8d2821865974a0121
J. Alberto Pilar <jesuspilarsantana@gmail.com>
2022-04-05 19:54:16 +0100

    Refactory auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,47 @@
-const router = require("express").Router();
+const bcrypt = require("bcrypt"); // require to encrypt passwords
+const jwt = require("jsonwebtoken"); // required to generate a token
 
-const { signup, login } = require("../controllers/auth.controller");
+const UserModel = require("../models/users.model"); // bring user model
 
-router.post("/signup", signup);
-router.post("/login", login);
+async function signup(req, res) {
+  try {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    const user = await UserModel.create(req.body);
+    const payload = { email: user.email };
+    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: "1h" });
+    res.status(200).json({ email: user.email, token });
+  } catch (error) {
+    res.status(500).send(`Error creating user: ${error}`);
+  }
+}
 
-module.exports = router;
+async function login(req, res) {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      res.status(400).send("Email or password incorrect");
+      return;
+    }
+
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (err) throw new Error(err);
+
+      if (!result) {
+        return res.json({ error: "Email or password incorrect" });
+      }
+
+      const token = jwt.sign({ email: user.email }, process.env.SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.status(200).json({ email: user.email, token });
+    });
+  } catch (error) {
+    res.status(500).send("Error logging user");
+  }
+}
+
+module.exports = {
+  signup,
+  login,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 
 const genUserRouter = require('./users.router')
 const genPropertyRouter = require('./property.router')
+const genAuthRouter = require('./auth.router')
 
 router
   .use('/user', genUserRouter)
   .use('/property', genPropertyRouter)
+  .use('/auth', genAuthRouter)
 
 module.exports = router
